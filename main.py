85cd6f4107b3ae8f64f70609674e6905ce0dde92
Ayush <ayush.ayush.kr@gmail.com>
2016-10-16 23:51:38 +0530

    html as template

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import webapp2
+import cgi
 form="""
 
+<html>
+  <head>
+    <title>Unit 2 Rot 13</title>
+  </head>
 
-def escape_html(s):
+  <body>
+    <h2>Enter some text to ROT13:</h2>
+    <form method="post" action="/testform">
+      <textarea name="text"
+                style="height: 100px; width: 400px;"></textarea>
+      <br>
+      <input type="submit">
+    </form>
+  </body>
 
- for (i,o) in (("&", "&amp;"),
-              ('"',  "&quote;"),
-              ("<", "&lt;"),
-              (">", "&gt;")):
-    s=s.replace(i,o)
- return s
-print escape_html("escaping added")
+</html>
+"""
 
 class MainPage(webapp2.RequestHandler):
  def get(self):
      self.response.out.write(form)
-app=webapp2.WSGIApplication([('/',MainPage)])
+ def escape_html(s):
+     return cgi.escape(text, quote=True)
+class TestHandler(webapp2.RequestHandler):
+ def post(self):
+     text=self.request.get("text")
+     q= escape_html(text)
+     self.response.out.write(q)
+app=webapp2.WSGIApplication([('/',MainPage),('/testform',TestHandler)],
+                            debug=True)
